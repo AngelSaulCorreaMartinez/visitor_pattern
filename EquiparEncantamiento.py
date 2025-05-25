d21c6f51351a5d61840534e61de599267583b881
ASCM5 <utm22030590@utma.edu.mx>
2025-05-25 11:08:23 -0600

    Agregar documentación y comentarios en las clases de visitantes y personajes

--- EquiparEncantamiento.py
+++ EquiparEncantamiento.py
@@ -1,15 +1,23 @@
+# Visitante concreto que equipa encantamientos a los magos
 from Visitante import Visitante
 
 class EquiparEncantamiento(Visitante):
+    """
+    Implementación concreta de un visitante que equipa encantamientos
+    solo a los magos, basándose en su nivel mágico
+    """
     def visitar_mago(self, mago):
+        """Equipa un encantamiento según el nivel mágico del mago"""
         if mago.get_nivel_magico() <= 5:
-            mago.set_encantamiento('Bola de fuego')
+            mago.set_encantamiento('Bola de fuego')  # Encantamiento básico
         else:
-            mago.set_encantamiento('Rayo de hielo')
+            mago.set_encantamiento('Rayo de hielo')  # Encantamiento avanzado
         
     def visitar_guerrero(self, guerrero):
+        """Los guerreros no reciben encantamientos"""
         pass
 
     def visitar_personajes(self, personajes):
+        """Visita cada personaje para equipar encantamientos si corresponde"""
         for personaje in personajes:
-            personaje.accept(self)
+            personaje.aceptar(self)
--- Main.py
+++ Main.py
@@ -1,24 +1,45 @@
+# Importamos las clases necesarias para nuestro juego
 from Guerrero import Guerrero
 from Mago import Mago
 from EquiparArma import EquiparArma
 from EquiparEncantamiento import EquiparEncantamiento
 
 def main():
+    # Creamos instancias de nuestros personajes
     g1 = Guerrero()
     g2 = Guerrero()
     m1 = Mago()
-    m2 = Mago()
+    m2 = Mago()    # Configuramos los niveles mágicos de los magos
+    m1.set_nivel_magico(3)  # Mago con nivel bajo (recibirá Bola de Fuego)
+    m2.set_nivel_magico(7)  # Mago con nivel alto (recibirá Rayo de Hielo)
 
-    m1.set_nivel_magico(3)
-    m2.set_nivel_magico(7)
-
+    # Creamos la lista de personajes
     personajes = []
-    personajes.append([g1, g2, m1, m2])
+    personajes.extend([g1, g2, m1, m2])
 
+    # Creamos y aplicamos el visitante para equipar armas
     ea = EquiparArma()
     ea.visitar_personajes(personajes)
 
     ee = EquiparEncantamiento()
     ee.visitar_personajes(personajes)
 
+    mostrar_info(personajes)
+
+def mostrar_info(personajes):
+    """
+    Función que muestra la información de cada personaje.
+    Para todos los personajes muestra su arma.
+    Para los magos además muestra su encantamiento y nivel mágico.
+    """
+    for personaje in personajes:
+        # Mostramos el arma que tiene equipada (todos los personajes tienen arma)
+        print(f'Arma: {personaje.get_arma()}')
+    
+        # Verificamos si el personaje es una instancia de Mago para mostrar info adicional
+        if isinstance(personaje, Mago):
+            print(f'Encantamiento: {personaje.get_encantamiento()}')
+            print(f'Nivel Magico: {personaje.get_nivel_magico()}')
+
+            print('')
 main()
